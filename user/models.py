56753ea018c71534f82bd0bad2331a7a8e04e0a5
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-04-20 20:58:41 +0200

    added Show Model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Show(models.Model):
+    """Show to be shared by the users"""
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_lenght=255)
+    user = models.Foreignkey(User, on_delete=models.CASCADE)
