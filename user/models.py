3beb5f810d0b5350db325631efe6abbdb7ee674f
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-04-21 20:00:28 +0200

    user model and URLField

--- user/models.py
+++ user/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
+
+
+class User(AbstractBaseUser):
+    """Custom User Model"""
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=255)
+    netflix_login = models.CharField(max_length=255)
+    netflix_username = models.CharField(max_length=50)
+    netflix_password = models.CharField(max_length=30)
+
 
 class Show(models.Model):
     """Show to be shared by the users"""
     title = models.CharField(max_length=255)
-    link = models.CharField(max_lenght=255)
+    link = models.URLField(max_length=255)
     user = models.Foreignkey(User, on_delete=models.CASCADE)
